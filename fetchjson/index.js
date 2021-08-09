dde0dc44e2de33527ae1ef18a03dcfb748a22e2f
Alec Tecson <tecsonalr@gmail.com>
2021-08-09 17:01:29 +0800

    Catching Errors with Typescript

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,9 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log("\n        The todo with ID: " + id + "\n        Has a title of: " + title + "\n        Is it finished?: " + completed + "\n    ");
 });
