219ce07dc0167999608056151799afeb792790bf
Alec Tecson <tecsonalr@gmail.com>
2021-08-10 17:41:54 +0800

    Void and Never

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -14,3 +14,24 @@ const multiply = function (a: number, b: number): number { // anonymous
     return a * b;
 }
 
+const logger = (message: string): void => {
+    console.log(message);
+}
+
+// const throwError = (message: string): never => {
+//     throw new Error(message);
+// }
+
+// const throwError = (message: string): string => {
+//     if (!message) {
+//         throw new Error(message);
+//     }
+    
+//     return message;
+// }
+
+const throwError = (message: string): void => {
+    if (!message) {
+        throw new Error(message);
+    }
+}
