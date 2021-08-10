c2f709cc284e14b83929ee40a003e0f6cc15de84
Alec Tecson <tecsonalr@gmail.com>
2021-08-10 17:34:16 +0800

    Annotations for Anonymous Functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,16 @@
-const add = (a: number, b: number): number => {
+const add = (a: number, b: number) => {
     return a + b;
 }
+
+const subtract = (a: number, b: number): number => { // arrow
+    return a - b;
+}
+
+function divide(a: number, b: number):number { // named
+    return a / b;
+}
+
+const multiply = function (a: number, b: number): number { // anonymous
+    return a * b;
+}
+
