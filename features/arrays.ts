69d0ef6c81189ffda1db05e42983af38c856f3e0
Alec Tecson <tecsonalr@gmail.com>
2021-08-11 14:52:23 +0800

    Arrays in Typescript

---
+++ features/arrays.ts
@@ -0,0 +1,9 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+// const carMakers: string[] = [];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+]
