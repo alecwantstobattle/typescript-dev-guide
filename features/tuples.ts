0e74cf708487d77fed9655364de808ec8620d9f2
Alec Tecson <tecsonalr@gmail.com>
2021-08-12 14:50:12 +0800

    Why Tuples?

--- features/tuples.ts
+++ features/tuples.ts
@@ -8,9 +8,17 @@ const drink = {
 // pepsi[0] = 40;
 // pepsi[2] = 'brown';
 
+// Type alias
 type Drink = [string, boolean, number];
 
 // const pepsi: [string, boolean, number] = ['brown', true, 40];
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 40];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+    horsePower: 400,
+    weight: 3354
+}
