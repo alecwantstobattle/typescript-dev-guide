450c515ad9f56b2774c81b64264f643d678d1ef1
Alec Tecson <tecsonalr@gmail.com>
2021-08-16 18:15:48 +0800

    Updating Interface Definitions

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -8,7 +8,8 @@ interface Mappable {
     location: {
         lat: number;
         lng: number;
-    }
+    };
+    markerContent(): string;
 }
 
 export class CustomMap {
@@ -35,7 +36,7 @@ export class CustomMap {
 
         marker.addListener('click', () => {
             const infoWindow = new google.maps.InfoWindow({
-                content: 'Hi there'
+                content: mappable.markerContent()
             })
 
             infoWindow.open(this.googleMap, marker)
